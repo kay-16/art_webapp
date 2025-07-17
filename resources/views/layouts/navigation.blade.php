3afd019d9dccabe0402b0c7059e6f44b55fb6d28
Kyla Reambonanza <20221465@gmail.com>
2025-07-17 17:37:22 +0800

    Updated Profile Dropdown Style

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,13 +11,13 @@
                     {{ __('Home') }}
                 </x-nav-link>
 
-                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                <x-nav-link :href="route('upload')" :active="request()->routeIs('upload')">
                     {{ __('Upload') }}
                 </x-nav-link>
 
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                        <button class="inline-flex font-neuton font-[300] text-white items-center text-xl hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ms-1">
