ef74a0b8c7608000e92ad3e62927ff63589c45f2
Kyla Reambonanza <20221465@gmail.com>
2025-07-24 23:58:02 +0800

    Login Page Styling - almost done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,7 +34,7 @@
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-xs text-black hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                <a class="underline text-xs text-black hover:text-black rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#FFFDD0]" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'place-content-center w-1/2 custom-border custom-border:focus text-[#841A26] focus:outline-none placeholder:text-[#841A26]/50 font-neuton']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'appearance-none bg-transparent place-content-center w-1/2 custom-border custom-border:focus text-[#841A26] focus:outline-none focus:outline-none focus:ring-0 placeholder:text-[#841A26]/50 font-neuton']) }}>
