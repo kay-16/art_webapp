43cdac01e2e4f2165b934e584afcbf07b0b8ffc2
Kyla Reambonanza <20221465@gmail.com>
2025-08-13 20:22:41 +0800

    Imported PostsController; Removed Upload route; Added route declaration for Posts; Modified Dashboard route view to 'profile.index'

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,17 +10,19 @@ Route::get('/', function () {
 
 Route::get('/profile/{user}', [ProfileController::class, 'show']);
 
+Route::resource('/posts', PostsController::class);  
+
 Route::get('/dashboard', function () {
-    return view('dashboard', ['user' => Auth::user()]);
+    return view('profile.index', ['user' => Auth::user()]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/upload', function () {
-        return view('upload');
-    })->name('upload');
+    
 });
 
 require __DIR__.'/auth.php';
+
+
