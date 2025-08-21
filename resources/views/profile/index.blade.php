bb927fc8da64500a98bac200d52b1af72a2862bd
Kyla Reambonanza <20221465@gmail.com>
2025-08-21 22:47:06 +0800

    Display image, title, year, and type of each user in index; Modified link to Add New Post button link

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -4,7 +4,7 @@
             <h2 class="font-neuton font-[400] text-3xl">
                 {{ $user->username }}'s Gallery
             </h2>
-            <a href="#" class="font-neuton font-[400] text-base underline underline-offset-1">Add New Post</a>
+            <a href="/posts/create" class="font-neuton font-[400] text-base underline underline-offset-1">Add New Post</a>
         </div>
 
         <div><p class="font-neuton font-[400] text-base">{{ $user->profile->bio }}</p></div>
@@ -18,10 +18,15 @@
     </x-slot>
 
     <div class="grid grid-cols-4 gap-4 max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <img src="{{ asset('img_1.jpeg') }}" class="w-60" alt="">
-        <img src="{{ asset('img_2.jpeg') }}" class="w-60" alt="">
-        <img src="{{ asset('img_3.jpeg') }}" class="w-60" alt="">
-        <img src="{{ asset('img_4.jpeg') }}" class="w-60" alt="">
+        @foreach($user->posts as $post)
+            
+            <div class="flex flex-col items-start">
+                <img src="/storage/{{ $post->image }}" class="w-60" alt="">
+                <p class="font-neuton font-[400] text-base mt-2"> {{ $post->title }}</p>
+                <p class="font-neuton font-[400] text-base"> {{ $post->year }}</p>
+                <p class="font-neuton font-[400] text-base"> {{ $post->type }}</p>
+            </div>
+        @endforeach
     </div>
 </x-app-layout>
 
