043aefadd6d21198ff50c3e5924b4430b2196c8f
Kyla Reambonanza <20221465@gmail.com>
2025-08-24 01:38:35 +0800

    Enhanced UI styling of user profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,34 +1,63 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="mt-6 flex justify-between items-baseline leading-tight border-b-2 border-[#841A26]">
-            <h2 class="font-neuton font-[400] text-3xl">
-                {{ $user->username }}'s Gallery
-            </h2>
-            <a href="/posts/create" class="font-neuton font-[400] text-base underline underline-offset-1">Add New Post</a>
+        <!-- User profile -->
+        <div class="mt-10 ml-20 mr-16 flex gap-[14rem]">
+            <!-- Profile picture  -->
+            <div class="w-[18rem] h-[18rem] rounded-full bg-[#E0D9AB]"></div>
+
+            <!-- User Info -->
+            <div class="flex flex-col flex-1 leading-tight">
+                <div class="flex justify-between items-baseline border-b-2 border-[#841A26]">
+                    <!-- Name of user -->
+                    <h2 class="font-neuton font-[400] text-3xl">{{ $user->username }}'s Gallery</h2>
+
+                    <!-- Add new post button -->
+                    <a href="/posts/create" class="font-neuton font-[400] text-base underline underline-offset-1">Add New Post</a>
+                </div>
+
+                <!-- Stats row -->
+                <div class="mt-3 flex justify-between gap-10">
+                    <p class="font-nokora font-[400] text-base"> 120 followers</p>
+                    <p class="font-nokora font-[400] text-base">{{ $user->posts->count() }} posts</p>
+                </div>
+
+                <!-- Bio -->
+                <div class="mt-3 max-w-xl">
+                    <p class="font-neuton font-[400] text-base">{{ $user->profile->bio }}</p>   
+                </div>
+
+                <!-- Social links -->
+                <div>
+                    
+                </div>
+            </div>
+        
+            
+
         </div>
 
-        <div><p class="font-neuton font-[400] text-base">{{ $user->profile->bio }}</p></div>
+        
 
-        <div><p class="font-neuton font-[400] text-base">{{ $user->posts->count() }} posts</p></div>
+        
 
         <div>
-            <a class="font-neuton font-[400] text-base underline underline-offset-1" href="https://www.instagram.com/bykaysart?igsh=MWw3ZXM4YW52dGJwZQ==" target="_blank">
+            <a class="font-neuton font-[400] text-base underline underline-offset-1" href="https://www.instagram.com/bykaysart" target="_blank">
                 {{ $user->profile->url ?? 'N/A'}}
             </a>
         </div>
 
     </x-slot>
 
-    <div class="grid grid-cols-4 gap-4 max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+    <div class="mt-[7rem] grid grid-cols-4 gap-4 max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
         @foreach($user->posts as $post)
             
             <div class="flex flex-col items-start">
                 <a href="/posts/{{ $post->id }}">
                     <img src="/storage/{{ $post->image }}" class="w-60" alt="">
                 </a>
-                <p class="font-neuton font-[400] text-base mt-2"> {{ $post->title }}</p>
-                <p class="font-neuton font-[400] text-base"> {{ $post->year }}</p>
-                <p class="font-neuton font-[400] text-base"> {{ $post->type }}</p>
+                <p class="font-neuton font-[400] text-[22px] mt-2 text-[#841A26]"> {{ $post->title }}</p>
+                <p class="font-nokora font-[300] text-lg text-[#841A26]"> {{ $post->type }}</p>
+                <p class="font-nokora font-[200] text-sm text-[#841A26]"> {{ $post->year }}</p>
             </div>
         @endforeach
     </div>
