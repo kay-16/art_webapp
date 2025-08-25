633c91a7b034decb89c1cf399cc496be4343591f
Kyla Reambonanza <20221465@gmail.com>
2025-08-25 23:49:26 +0800

    More index UI styling enhanced for user profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,13 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
         <!-- User profile -->
-        <div class="mt-10 ml-20 mr-16 flex gap-[14rem]">
+        <div class="mt-10 ml-20 mr-16 flex gap-[13rem]">
             <!-- Profile picture  -->
             <div class="w-[18rem] h-[18rem] rounded-full bg-[#E0D9AB]"></div>
 
             <!-- User Info -->
             <div class="flex flex-col flex-1 leading-tight">
                 <div class="flex justify-between items-baseline border-b-2 border-[#841A26]">
+
                     <!-- Name of user -->
                     <h2 class="font-neuton font-[400] text-3xl">{{ $user->username }}'s Gallery</h2>
 
@@ -21,34 +22,25 @@
                     <p class="font-nokora font-[400] text-base">{{ $user->posts->count() }} posts</p>
                 </div>
 
-                <!-- Bio -->
-                <div class="mt-3 max-w-xl">
+                <!-- More info of user -->
+                <div class="flex justify-between mt-3 max-w-xl">
+                    <!-- Bio -->
                     <p class="font-neuton font-[400] text-base">{{ $user->profile->bio }}</p>   
-                </div>
 
-                <!-- Social links -->
-                <div>
-                    
-                </div>
+                    <!-- Social links -->
+                    <div class="flex flex-col items-end gap-1">
+                        <a href="{{ $user->profile->url ?? '#'}}"
+                            target="_blank"
+                            class="items-centerborder-4 bg-[#841A26] border-none px-7 py-[0.8px] text-lg text-[#FEFBDC]"> 
+                            Socials
+                        </a>
+                    </div> 
+                </div> 
             </div>
-        
-            
-
         </div>
-
-        
-
-        
-
-        <div>
-            <a class="font-neuton font-[400] text-base underline underline-offset-1" href="https://www.instagram.com/bykaysart" target="_blank">
-                {{ $user->profile->url ?? 'N/A'}}
-            </a>
-        </div>
-
     </x-slot>
 
-    <div class="mt-[7rem] grid grid-cols-4 gap-4 max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+    <div class="mt-[7rem] grid grid-cols-4 gap-4 max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 border-t-2 border-[#841A26]">
         @foreach($user->posts as $post)
             
             <div class="flex flex-col items-start">
