c63ba22de07a42426838a82c9f07dc1c6d3da41c
Kyla Reambonanza <20221465@gmail.com>
2025-09-21 23:48:33 +0800

    Render post details (image, type, year) in the display posts.show view

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,3 +1,20 @@
 <x-app-layout>
+<div class="container">
+    <section class="hero container max-w-screen-lg mx-auto flex justify-center">
+        <img src="/storage/{{$post->image}}" class="mx-auto w-50 h-50" alt="image">
+
+        <div class="container">
+            <div>
+                <h3>{{$post->title}}</h3>
+
+                <p>{{$post->type}}</p>
+                <p>{{$post->year}}</p>
+
+            </div>
+    </div>
+    </section>
+
+    
+</div>
 
 </x-app-layout>
